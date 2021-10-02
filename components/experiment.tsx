a1f5c72c705e2543a79a8761f08af521c18748bc
Advaith <advaithj1@gmail.com>
2021-10-01 17:13:10 -0700

    add control support

--- components/experiment.tsx
+++ components/experiment.tsx
@@ -10,23 +10,31 @@ export default function Experiment({exp}: {exp: Exp}) {
 		<h2 style={{fontSize: '1.5rem', marginTop: '5px', marginBottom: '5px'}}>{exp.data.title}</h2>
 		<p style={{fontSize: '.9rem'}}>{exp.data.id}</p>
 		<div>
-			{exp.rollout[3].map((population, i) => <div key={i} style={{background: '#202225', padding: '.5rem', marginBottom: '.25rem', borderRadius: '2px'}}>
-				{population[1][0] && <>
-					<p style={{fontSize: '.9rem', margin: 0}}>Filter: {list.format(population[1].map(f => parseFilter(f)))}</p>
-					<hr style={{border: 0, height: '1px', background: '#2f3136'}}/>
-				</>}
-				{population[0].map(bucket => <div key={bucket[0]}>
-					{exp.data.description.find(d => d.startsWith(`Treatment ${bucket[0]}`)) ?? 'None'}:{' '}
-						{bucket[1].reduce((total, range) => total + range.e - range.s, 0)/100}%
+			{exp.rollout[3].map((population, i) => {
+				let popTotal = 0
+				return <div key={i} style={{background: '#202225', padding: '.5rem', marginBottom: '.25rem', borderRadius: '2px'}}>
+					{population[1][0] && <>
+						<p style={{fontSize: '.9rem', margin: 0}}>Filter: {list.format(population[1].map(f => parseFilter(f)))}</p>
+						<hr style={{border: 0, height: '1px', background: '#2f3136'}}/>
+					</>}
+					{population[0].map(bucket => <div key={bucket[0]}>
+						{exp.data.description.find(d => d.startsWith(`Treatment ${bucket[0]}`)) ?? 'None'}:{' '}
+						{(() => {
+							const sum = bucket[1].reduce((total, range) => total + range.e - range.s, 0)
+							popTotal += sum
+							return sum
+						})() / 100}%
 						({bucket[1].map(range => `${range.s} - ${range.e}`).join(', ')})
-				</div>)}
-			</div>)}
+					</div>)}
+					{popTotal < 10_000 && `${exp.data.description[0]}: ${(10_000 - popTotal)/100}%`}
+				</div>
+			})}
 		</div>
 		<button onClick={() => swal.fire({
 			title: exp.data.title,
 			html: <>{exp.data.buckets.map(b => 
 				<div key={b}>
-					<p style={{marginBottom: '.5rem'}}>{exp.data.description.find(d => d.startsWith(`Treatment ${b}`))}</p>
+					<p style={{marginBottom: '.5rem'}}>{b ? exp.data.description.find(d => d.startsWith(`Treatment ${b}`)) : exp.data.description[0]}</p>
 					{exp.rollout[4].find(r => r.b === b) && <details style={{fontSize: '.9rem'}}>
 						<summary style={{cursor: 'pointer'}}>{exp.rollout[4].find(r => r.b === b).k.length} Override{exp.rollout[4].find(r => r.b === b).k.length === 1 ? '' : 's'}</summary>
 						{exp.rollout[4].find(r => r.b === b).k.map(id => <p key={id} style={{margin: '.25rem 0 .25rem .4rem'}}>{id}</p>)}
